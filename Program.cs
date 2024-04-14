ed1f2a9179c90164a6caed8fa92f64dd9c2204c1
Matheus Castello <matheus.castello@toradex.com>
2024-04-14 18:06:23 +0200

    Initial Commit
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Slint;
+using AppWindow;
+using Torizon.Shell;
+
+var win = new Window();
+
+Console.WriteLine("Hello Torizon!");
+
+var startEmulatorFunc = (int ram, int storage, int instances) => {
+    Exec.Bash(
+        $"cd ./assets && " +
+        $"RAM={ram} " +
+        $"STORAGE={storage} " +
+        $"INSTANCES={instances} " +
+        $"docker compose run --rm --service-ports emulator"
+    );
+};
+
+win.StartEmulator = () => {
+    Console.WriteLine("Starting Emulator");
+
+    startEmulatorFunc(
+        int.Parse(win.ramSize.Replace("\"", "")),
+        int.Parse(win.storageSize.Replace("\"", "")),
+        int.Parse(win.instances.Replace("\"", ""))
+    );
+
+    Environment.Exit(0);
+};
+
+win.Run();
