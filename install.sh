7097ce20793fa8f7e15fe8b15c35785ebf9867ec
Matheus Castello <matheus.castello@toradex.com>
2024-04-15 13:22:28 +0200

    github: Fix install link to the commontorizon repo and add it to release
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ VERSION="0.0.3"
 echo "Installing Torizon Emulator Manager ..."
 
 sudo apt-get install libicu72 zlib1g unzip
-wget https://github.com/microhobby/torizonEmulatorManager/releases/download/$VERSION/Torizon-Emulator-Manager-$VERSION.zip
+wget https://github.com/commontorizon/torizonEmulatorManager/releases/download/$VERSION/Torizon-Emulator-Manager-$VERSION.zip
 sudo mkdir -p /opt/torizon-emulator-manager
 sudo unzip -o Torizon-Emulator-Manager-$VERSION.zip -d /opt/torizon-emulator-manager
 sudo ln -sf /opt/torizon-emulator-manager/bin/Release/net8.0/linux-x64/publish/torizonEmulatorManager /usr/bin/torizon-emulator-manager
