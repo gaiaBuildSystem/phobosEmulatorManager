9072d4b06c86447aaccd338f066022cd342737cf
Matheus Castello <matheus.castello@toradex.com>
2024-04-15 10:32:24 +0200

    install: Bump version to 0.0.3 and fix overwrite files
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-VERSION="0.0.2"
+VERSION="0.0.3"
 
 echo "Installing Torizon Emulator Manager ..."
 
 sudo apt-get install libicu72 zlib1g unzip
 wget https://github.com/microhobby/torizonEmulatorManager/releases/download/$VERSION/Torizon-Emulator-Manager-$VERSION.zip
 sudo mkdir -p /opt/torizon-emulator-manager
-sudo unzip Torizon-Emulator-Manager-$VERSION.zip -d /opt/torizon-emulator-manager
-sudo ln -s /opt/torizon-emulator-manager/bin/Release/net8.0/linux-x64/publish/torizonEmulatorManager /usr/bin/torizon-emulator-manager
+sudo unzip -o Torizon-Emulator-Manager-$VERSION.zip -d /opt/torizon-emulator-manager
+sudo ln -sf /opt/torizon-emulator-manager/bin/Release/net8.0/linux-x64/publish/torizonEmulatorManager /usr/bin/torizon-emulator-manager
+rm Torizon-Emulator-Manager-$VERSION.zip
 
 echo "Now run torizon-emulator-manager command to start the application."
