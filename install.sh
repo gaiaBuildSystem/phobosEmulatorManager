a3dcea2d820162e331d018d470fafd9d4bf0f315
Matheus Castello <matheus.castello@toradex.com>
2024-04-23 13:30:40 -0300

    install: Download the files to the /tmp
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- install.sh
+++ install.sh
@@ -4,17 +4,20 @@ VERSION="0.0.9"
 
 echo "📦 Installing Torizon Emulator Manager ..."
 
-wget https://github.com/commontorizon/torizonEmulatorManager/releases/download/$VERSION/torizon-emulator-manager
-wget https://github.com/commontorizon/torizonEmulatorManager/releases/download/$VERSION/docker-compose.yml
-wget wget https://github.com/commontorizon/torizonEmulatorManager/releases/download/$VERSION/docker-compose.wsl.yml
+wget -O /tmp/torizon-emulator-manager \
+    https://github.com/commontorizon/torizonEmulatorManager/releases/download/$VERSION/torizon-emulator-manager
+wget -P /tmp/docker-compose.yml \
+    https://github.com/commontorizon/torizonEmulatorManager/releases/download/$VERSION/docker-compose.yml
+
+echo "📦 Moving Assets ..."
 
 sudo mkdir -p /opt/torizon-emulator-manager
-sudo mv torizon-emulator-manager /opt/torizon-emulator-manager
-sudo mv docker-compose.yml /opt/torizon-emulator-manager
+sudo mv /tmp/torizon-emulator-manager /opt/torizon-emulator-manager
+sudo mv /tmp/docker-compose.yml /opt/torizon-emulator-manager
 sudo chmod +x /opt/torizon-emulator-manager/torizon-emulator-manager
 sudo ln -sf /opt/torizon-emulator-manager/torizon-emulator-manager /usr/bin/torizon-emulator-manager
 
-echo "🛜 Downloading the image ..."
+echo "🛜 Pre-downloading the image ..."
 docker compose -f /opt/torizon-emulator-manager/docker-compose.yml pull
 
 echo "🎉 Torizon Emulator Manager installed successfully!"
